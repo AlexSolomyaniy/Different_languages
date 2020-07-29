5c47babecef53bd7e7d2804cac0863e975df4656
alex <alexsolomyaniy@gmail.com>
2020-07-29 19:27:24 +0300

    added language tests

--- conftest.py
+++ conftest.py
@@ -5,21 +5,21 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language_name', action='store', default=None,
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: espanish or franch")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    language_name = request.config.getoption("language_name")
+    language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        if language_name == "es":
+        if language == "es":
             print("\nchoosed spahish language...")
             options = Options()
             options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})
             browser = webdriver.Chrome(options=options)
-        elif language_name == "fr":
+        elif language == "fr":
             print("\nchoosed franch language...")
             options = Options()
             options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})
@@ -28,12 +28,12 @@ def browser(request):
             raise pytest.UsageError("--language_name should be spanish or franch")
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        if language_name == "es":
+        if language == "es":
             print("\nchoosed spahish language...")
             fp = webdriver.FirefoxProfile()
             fp.set_preference("intl.accept_languages", "es")
             browser = webdriver.Firefox(firefox_profile=fp)
-        elif language_name == "fr":
+        elif language == "fr":
             print("\nchoosed franch language...")
             fp = webdriver.FirefoxProfile()
             fp.set_preference("intl.accept_languages", "fr")
